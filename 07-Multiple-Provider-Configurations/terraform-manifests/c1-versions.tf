09735f8cc37f72e20d895c550c4cfc2cde7d46f6
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2022-03-01 19:45:46 +0530

    Multi-provider practice

--- 07-Multiple-Provider-Configurations/terraform-manifests/c1-versions.tf
+++ 07-Multiple-Provider-Configurations/terraform-manifests/c1-versions.tf
@@ -11,21 +11,19 @@ terraform {
 
 # Provider-1 for EastUS (Default Provider)
 provider "azurerm" {
-  features {}
+  features{}
 }
+
 
 # Provider-2 for WestUS
 provider "azurerm" {
-  features {
+  features{
     virtual_machine {
-      delete_os_disk_on_deletion = false # This will ensure when the Virtual Machine is destroyed, Disk is not deleted, default is true and we can alter it at provider level
+      delete_os_disk_on_deletion = false
     }
   }
-  alias = "provider2-westus"
-  #client_id = "XXXX"
-  #client_secret = "YYY"
-  #environment = "german"
-  #subscription_id = "JJJJ"
+
+  alias = "provider2-eastus"
 }
 
 
--- 07-Multiple-Provider-Configurations/terraform-manifests/c2-resource-group.tf
+++ 07-Multiple-Provider-Configurations/terraform-manifests/c2-resource-group.tf
@@ -1,16 +1,14 @@
 # Create a resource group in EastUS region - Uses Default Provider
-resource "azurerm_resource_group" "myrg1" {
-  name = "myrg-1"
-  location = "East US"
+resource "azurerm_resource_group" "eastus" {
+  name = "eastusresource"
+  location = "eastus"
 }
-
 #Create a resource group in WestUS region - Uses "provider2-westus" provider
-resource "azurerm_resource_group" "myrg2" {
-  name = "myrg-2"
-  location = "West US"
-  provider = azurerm.provider2-westus
+resource "azurerm_resource_group" "eastus2" {
+  name = "eastusresource2"
+  location = "eastus2"
+  provider = azurerm.provider2-eastus
 }
-
 
 /*
 Additional Note: 
